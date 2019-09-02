9234c9bbee9b4237b94377f6e29e23e680c6dd1f
jlarsen16 <jadynjlarsen@hotmail.com>
2019-09-02 11:37:45 -0500

    Initial commit

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Jadyn Larsen
+ * Date: 2019/09/02
+ *
+ * A hello world program for hack01
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Hello World!\n");
+
+  return 0;
+}
